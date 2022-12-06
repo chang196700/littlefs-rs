99b446a72810b95bd85c1527c7443d2b416e7027
Chang Feng <chang_196700@hotmail.com>
2022-12-06 17:43:07 +0800

    add features

--- littlefs-rs-sys/build.rs
+++ littlefs-rs-sys/build.rs
@@ -2,30 +2,47 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> anyhow::Result<()> {
+    println!("cargo:rerun-if-changed=build.rs");
+
     let target = env::var("CLANG_TARGET")
         .or_else(|_| env::var("TARGET"))?;
 
-    if cfg!(not(feature = "nolib")) {
+    let definitions = vec![
+        #[cfg(not(feature = "malloc"))]
+        "LFS_NO_MALLOC",
+        #[cfg(not(feature = "debug"))]
+        "LFS_NO_DEBUG",
+        #[cfg(not(feature = "warn"))]
+        "LFS_NO_WARN",
+        #[cfg(not(feature = "error"))]
+        "LFS_NO_ERROR",
+        #[cfg(not(feature = "assert"))]
+        "LFS_NO_ASSERT",
+        #[cfg(feature = "trace")]
+        "LFS_YES_TRACE",
+    ];
+
+    if cfg!(not(feature = "no_lib")) {
         let mut builder = cc::Build::new();
         let builder = builder
             .flag("-std=c11")
-            .flag("-DLFS_NO_MALLOC")
-            .flag("-DLFS_NO_DEBUG")
-            .flag("-DLFS_NO_WARN")
-            .flag("-DLFS_NO_ERROR")
             .file("littlefs/lfs.c")
             .file("littlefs/lfs_util.c");
 
-        #[cfg(not(feature = "assertions"))]
-            let builder = builder.flag("-DLFS_NO_ASSERT");
-
-        #[cfg(feature = "trace")]
-            let builder = builder.flag("-DLFS_YES_TRACE");
+        for def in &definitions {
+            builder.flag(&format!("-D{}", def));
+        }
 
         builder.compile("lfs-sys");
     }
 
-    let bindings = bindgen::Builder::default()
+    let mut bindings = bindgen::Builder::default();
+
+    for def in &definitions {
+        bindings = bindings.clang_arg(&format!("-D{}", def));
+    }
+
+    let bindings = bindings
         .header("littlefs/lfs.h")
         .allowlist_file("littlefs/lfs.h")
         .allowlist_file("littlefs/lfs_util.h")
@@ -33,13 +50,17 @@ fn main() -> anyhow::Result<()> {
         .use_core()
         .ctypes_prefix("cty")
         .rustfmt_bindings(true)
-        .generate()
-        .expect("Unable to generate bindings");
+        .generate();
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    if let Ok(bindings) = bindings {
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    } else {
+        eprintln!("WARNING: No bindings generated!");
+        eprintln!("Error: {:?}", bindings);
+    }
 
     Ok(())
 }
